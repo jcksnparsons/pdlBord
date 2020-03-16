b7913664d826146773dd0629c84e32b51ab8297d
Jack Parsons <jcksnparsons@yahoo.com>
2020-03-16 14:12:04 -0500

    adding new options to sidebar in anticipation of future functionality

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { slide as Menu } from "react-burger-menu";
-import './Sidebar.css'
+import "./Sidebar.css";
 
 const SideBar = props => {
-
   return (
     <>
       <Menu>
+        <a className="menu-item" href="/board">
+          Pedalboard
+        </a>
         <a className="menu-item" href="/presets">
           Presets
+        </a>
+        <a className="menu-item" href="/login">
+          Logout
         </a>
       </Menu>
     </>
