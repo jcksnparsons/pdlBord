d75b96cb23a77fb6357119c39876fc2e3b2f7e3e
Jack Parsons <jcksnparsons@yahoo.com>
2020-03-16 10:51:33 -0500

    sidebar is present, needs styling and working links

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import logo from "./logo.svg";
 import * as Tone from "tone";
 import "./App.css";
 import Pedalboard from "./components/board/Pedalboard";
+import SideBar from "./components/sidebar/Sidebar";
 
 function App() {
-  
-
   return (
     <div className="App">
+      <SideBar />
       <Pedalboard />
     </div>
   );
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { slide as Menu } from "react-burger-menu";
+
+const SideBar = props => {
+  return (
+    <>
+      <Menu>
+        <a className="menu-item" href="/presets">
+          Presets
+        </a>
+      </Menu>
+    </>
+  );
+};
+
+export default SideBar;
