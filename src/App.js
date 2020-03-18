1e76182284d4942f3611ff5f329d610bcb54ae0c
Jack Parsons <jcksnparsons@yahoo.com>
2020-03-18 17:03:52 -0500

    new favicon, i know i know, not relevant right now

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom"
-import logo from "./logo.svg";
-import * as Tone from "tone";
 import "./App.css";
 import Views from "./components/Views"
 import SideBar from "./components/sidebar/Sidebar";
